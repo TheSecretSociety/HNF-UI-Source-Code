15039f00d4cbebe8a5f7748728ddb889a41e3511
bkfreeze <nguyen.bk95@gmail.com>
2017-03-21 10:19:56 +0700

    Create statictis
    Create new chart and display information for statistics

--- css/HNF-jquery.js
+++ css/HNF-jquery.js
@@ -116,3 +116,14 @@
     language: "vi"
 });
   });
+
+
+  (function(document) {
+      var _bars = [].slice.call(document.querySelectorAll('.bar-inner'));
+
+      _bars.map(function (bar, index) {
+          setTimeout(function() {
+            bar.style.width = bar.dataset.percent;
+        }, 0);
+      })
+  })(document);
