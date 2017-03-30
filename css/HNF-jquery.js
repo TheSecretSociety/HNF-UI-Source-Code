d0c296b30bc731726364dfef2c8b566acadffb7f
thuannguyen30 <kenzilian13@gmail.com>
2017-03-30 15:16:09 +0700

    Validation Update
    - Customer Detail Validation (Done)
    - Customer Contract Detail : minor improvement on UI
    - Detail Design Validation (WIP)
    - Production Detail : Remove Cancel Button and readjust position
    - Reformat Javascript
    -

--- css/HNF-jquery.js
+++ css/HNF-jquery.js
@@ -1,17 +1,33 @@
+// ***************************************************** //
+// *                ACTIVE ALL FUNCTION                * //
+// ***************************************************** //
+  // $(document).ready() only one so all function will be calling here
+    $(document).ready(function() {
+      LoginValidation();
+      EmployeePersonalValidation();
+      CustomerDetailValidation();
+      ContractDetailDesignValidation();
+
+      ClearButton();
+      DeleteButtonWarning();
+      ProductionProgressBar();
+      DatetimePicker();
+    });
 
   // ----------------------------------------------------- //
   // ---------------- GENERAL UI FUNCTION ---------------- //
   // ----------------------------------------------------- //
     // Clear button //
-    $(document).ready(function ClearButton(){
+    function ClearButton(){
         // script for Cancle Buttons
         $("#Cancle-Button").click(function(){
           $("input, textarea, select").val('').prop('checked', false);
           $("option").prop("selected", function(){return this.defaultSelected;});
         });
-    });
+      }
+
     // Delete Button Warning //
-    $(document).ready(function DeleteButtonWarning() {
+    function DeleteButtonWarning() {
       $('#EmployeeDeleteBtn').click(function() {
         DeleteWarning();
       })
@@ -31,9 +47,10 @@
           return false;
         }
       }
-    });
+    }
+
     // Production line Bar animation //
-    $(document).ready(function ProductionProgressBar(){
+    function ProductionProgressBar(){
       $('#check1').change(function () {
           // this will contain a reference to the checkbox
           if (this.checked) {
@@ -130,9 +147,10 @@
 
             }
         });
-    });
+    }
+
     // Datetimepicker //
-    $(document).ready(function DatetimePicker(){
+    function DatetimePicker(){
       $('#sandbox-container .input-group.date').datepicker({
         weekStart: 1,
         orientation: "bottom auto",
@@ -150,14 +168,13 @@
         autoclose: true,
         todayHighlight: true
       });
-    });
-
+    }
 
   // ----------------------------------------------------- //
   // --------------------- VALIDATION -------------------- //
   // ----------------------------------------------------- //
     // HNF-Login Validation //
-    $(document).ready(function LoginValidation(){
+    function LoginValidation(){
         $("#TxtLoginPassword").keyup(function(event){
           if(event.keyCode == 13){
               $("#LoginButton").click();
@@ -169,10 +186,10 @@
           }
         });
         $('#LoginButton').click(function() {
-          LoginValidate();
+          Validate();
         });
         // validate email and password
-        function LoginValidate(e) {
+        function Validate() {
           var PasswordValue = $('#TxtLoginPassword').val();
           var UsernameValue = $('#TxtLoginID').val();
           if (LoginUsernameValidate(UsernameValue) && LoginPasswordValidate(PasswordValue)){
@@ -180,24 +197,24 @@
             window.location = "./HNF-Dashboard.html";
           }else{
             alert("Login Fail");
-            e.preventDefault();
           }
         };
         // validate email format
         function LoginUsernameValidate(TxtLoginID){
           var EmailRegEx= /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-          console.log(EmailRegEx.test(TxtLoginID));
+          console.log('Email ID :' + EmailRegEx.test(TxtLoginID));
           return EmailRegEx.test(TxtLoginID);
         }
         // validate password format
         function LoginPasswordValidate(TxtLoginPassword) {
           var PasswordRegEx= /^.\S{1,}$/;
-          console.log(PasswordRegEx.test(TxtLoginPassword));
+          console.log('Password :' + PasswordRegEx.test(TxtLoginPassword));
           return PasswordRegEx.test(TxtLoginPassword);
         }
-      });
+      }
+
     // HNF-Employee-Personal Validation //
-    $(document).ready(function EmployeePersonalValidation(){
+    function EmployeePersonalValidation(){
       // trigger button
       $('#Personal-Update-Button').click(function() {
         EmployeePersonalValidate();
@@ -255,53 +272,89 @@
         console.log(OldPasswordRegEx.test(InputOldPassword,InputNewPassword));
         return OldPasswordRegEx.test(InputOldPassword,InputNewPassword);
       }
-    });
+    }
+
     // HNF-Customer-Detail Validation //
-    $(document).ready(function CustomerDetailValidation(){
-      $("#CustomerInfoSaveBtn").click(function() {
-        CustomerDetailValidate();
-      })
-      function CustomerDetailValidate() {
-        var CustomerName = $('#txt-CName').val();
-        var CustomerPhone = $('#txt-CPhone').val();
-        var CustomerEmail = $('#txt-CEmail').val();
-        if (CName_Format_Validate(CustomerName) &&  CPhone_Format_Validate(CustomerPhone) &&
-        CEmail_Format_Validate(CustomerEmail)){
-          alert("add Successfully");
-          return true;
-        }else{
-          alert("Invalid Input");
-          return false;
-        }
+    function CustomerDetailValidation(){
+      Validate();
+      function Validate() {
+        $('#txt-CName').change(function() {
+          var CustomerName = $('#txt-CName').val();
+          if (CName_Format_Validate(CustomerName)) {
+
+          }else{
+            $('#txt-CName').addClass('bg-warning');
+            window.setTimeout(function () {
+              $('#txt-CName').removeClass('bg-warning');
+            }, 1500);
+          }
+        })
+        $('#txt-CPhone').change(function() {
+          var CustomerPhone = $('#txt-CPhone').val();
+          if (CPhone_Format_Validate(CustomerPhone)) {
+
+          }else{
+            $('#txt-CPhone').addClass('bg-warning');
+            window.setTimeout(function () {
+              $('#txt-CPhone').removeClass('bg-warning');
+            }, 1500);
+          }
+        })
+        $('#txt-CEmail').change(function () {
+          var CustomerEmail = $('#txt-CEmail').val();
+          if (CEmail_Format_Validate(CustomerEmail)) {
+
+          }else {
+            $('#txt-CEmail').addClass('bg-warning');
+            window.setTimeout(function () {
+            $('#txt-CEmail').removeClass('bg-warning');
+            }, 1500);
+          }
+        })
+        $('#txt-CPrice').change(function() {
+          var numcheck = $('#txt-CPrice').val();
+          console.log("Price : " + numcheck);
+          $('#txt-CPrice').val(AntiNegativeValue(numcheck));
+        });
+        $('#txt-CQuantity').change(function() {
+          var numcheck = $('#txt-CQuantity').val();
+          $('#txt-CQuantity').val(AntiNegativeValue(numcheck));
+        });
+        $('#txt-CPriceRange').change(function() {
+          var numcheck = $('#txt-CPriceRange').val();
+          $('#txt-CPriceRange').val(AntiNegativeValue(numcheck));
+        })
       }
       function CName_Format_Validate(txtCustomerName) {
         var CustomerNameRegex = /^.[^.!@#$%^&*()_+-=]{0,35}$/;
-        console.log(CustomerNameRegex.test(txtCustomerName));
+        console.log('Customer Name : ' + CustomerNameRegex.test(txtCustomerName));
         return CustomerNameRegex.test(txtCustomerName);
       }
       function CPhone_Format_Validate(txtCustomerPhone) {
         var CustomerPhoneRegex = /^[\d]{2,13}$/;
-        console.log(CustomerPhoneRegex.test(txtCustomerPhone));
+        console.log('Customer Phone : '+CustomerPhoneRegex.test(txtCustomerPhone));
         return CustomerPhoneRegex.test(txtCustomerPhone);
       }
       function CEmail_Format_Validate(txtCustomerEmail) {
-
-        // if (typeof txtCustomerEmail === 'undefined' ||
-        //             txtCustomerEmail === null)
-        //
-        //             if (!txtCustomerEmail)
-
         var CustomerEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        console.log(CustomerEmail.test(txtCustomerEmail));
+        console.log('Customer Email : ' + CustomerEmail.test(txtCustomerEmail));
         return CustomerEmail.test(txtCustomerEmail);
       }
-    });
+      // make negative number into positive
+      function AntiNegativeValue(num) {
+        if (num < 0) {
+          num = -num;
+          return num;
+        }
+        return num;
+      }
+    }
 
     // HNF-Customer-ContractDetail-DetailDesign //
-    $(document).ready(function ContractDetailDesignValidation(){
-      DetailDesignValidate();
-      function DetailDesignValidate() {
-        // fast check quantity
+    function ContractDetailDesignValidation(){
+      Validate();
+      function Validate() { // this function will auto validate the input value everytime input has change
+        // Quantity Validate
         $("#txt_DD_Quantity").change(function TotalQuantityCheck() {
           var QuantityValue = Number($("#txt_DD_Quantity").val());
             $("#txt_DD_Quantity").val(AntiNegativeValue(QuantityValue)); // check negative value and return the value back to the input
@@ -356,8 +409,7 @@
           $('#txtFXXXLarge').val(AntiNegativeValue(FXXXLarge));
         });
         // Compare total shirt with total shirt size
-        $('#DDSubmitBtn').click(function CompareQuantity() {
-
+        $('#DDSubmitBtn').click(function CompareQuantity() { //only validation when "Submit Button" is click
           var MSmall = Number($('#txtMSmall').val());
           var MMedium = Number($('#txtMMedium').val());
           var MLarge = Number($('#txtMLarge').val());
@@ -374,7 +426,6 @@
           DDesign_Total_Quantity_Validate(MSmall,MMedium,MLarge,MXLarge,MXXLarge,MXXXLarge,FSmall,FMedium,FLarge,FXLarge,FXXLarge,FXXXLarge,TotalShirt);
         })
       }
-
       // make negative number into positive
       function AntiNegativeValue(num) {
         if (num < 0) {
@@ -415,14 +466,10 @@
           window.setTimeout(function () {
             $(".table-input").removeClass('bg-danger');
           }, 1000);
-
           return false;
-
         }
       }
-
-    });
-
+    }
 
 
   // ============= Statictis ============= //
